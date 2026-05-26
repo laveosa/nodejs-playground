bd24fd0158a1a758ea2454b58414a8a98ea8e82b
Nik <laveosa@yahoo.com>
2026-05-26 14:40:15 +0300

    Update - user-db-server refactor

--- src/utils/services/db/user-db-server.ts
+++ src/utils/services/db/user-db-server.ts
@@ -18,195 +18,168 @@ export default class UserDbServer {
   async getUser(id: string) {
     if (!id || id.length === 0) return null;
 
+    let response: UserModel;
+    let rawData: string;
+
     try {
-      let response: UserModel;
-      let rawData: string;
-
-      try {
-        rawData = (await FsService.readFile(USERS_JSON_PATH)) as string;
-      } catch (readError: any) {
-        if (readError?.code === "ENOENT") {
-          rawData = "[]";
-        } else {
-          throw readError;
-        }
+      rawData = (await FsService.readFile(USERS_JSON_PATH)) as string;
+    } catch (readError: any) {
+      if (readError?.code === "ENOENT") {
+        rawData = "[]";
+      } else {
+        throw readError;
       }
-
-      const users: UserModel[] = rawData ? JSON.parse(rawData) : [];
-      response = users.find((user) => user.id === id);
-
-      if (!response) throw new Error(`User with id: "${id}" not found`);
-
-      return response;
-    } catch (error) {
-      console.error("[API Server Internal Fetch Error]: ", error);
-      throw error;
     }
+
+    const users: UserModel[] = rawData ? JSON.parse(rawData) : [];
+    response = users.find((user) => user.id === id);
+
+    if (!response) throw new Error(`User with id: "${id}" not found`);
+
+    return response;
   }
 
   async getAllUsers() {
+    let rawData: string;
+
     try {
-      let responce: UserModel[];
-      let rawData: string;
-
-      try {
-        rawData = (await FsService.readFile(USERS_JSON_PATH)) as string;
-      } catch (readError: any) {
-        if (readError?.code === "ENOENT") {
-          rawData = "[]";
-        } else {
-          throw readError;
-        }
+      rawData = (await FsService.readFile(USERS_JSON_PATH)) as string;
+    } catch (readError: any) {
+      if (readError?.code === "ENOENT") {
+        rawData = "[]";
+      } else {
+        throw readError;
       }
-
-      responce = rawData ? (JSON.parse(rawData) as UserModel[]) : [];
-      return responce;
-    } catch (error) {
-      throw error;
     }
+
+    return rawData ? (JSON.parse(rawData) as UserModel[]) : [];
   }
 
   async createUser(data: UserModel) {
+    schemeValidation<UserModel>(UserScheme, data);
+
+    const newUser: UserModel = structuredClone<UserModel>(data);
+    let rawData: string;
+
     try {
-      schemeValidation<UserModel>(UserScheme, data);
-
-      const newUser: UserModel = structuredClone<UserModel>(data);
-      let rawData: string;
-
-      try {
-        rawData = (await FsService.readFile(USERS_JSON_PATH)) as string;
-      } catch (readError: any) {
-        if (readError.code === "ENOENT") {
-          rawData = "[]";
-        } else {
-          throw readError;
-        }
+      rawData = (await FsService.readFile(USERS_JSON_PATH)) as string;
+    } catch (readError: any) {
+      if (readError.code === "ENOENT") {
+        rawData = "[]";
+      } else {
+        throw readError;
       }
-
-      const users: UserModel[] = JSON.parse(rawData);
-      users.push(newUser);
-      const cleanUsers = removeModelDuplicate<UserModel>(users, ["id"]);
-
-      await FsService.writeFile(
-        USERS_JSON_PATH,
-        JSON.stringify(reassignIDs<UserModel>(cleanUsers), null, 2),
-      );
-      return data;
-    } catch (error) {
-      throw error;
     }
+
+    const users: UserModel[] = JSON.parse(rawData);
+    users.push(newUser);
+    const cleanUsers = removeModelDuplicate<UserModel>(users, ["id"]);
+
+    await FsService.writeFile(
+      USERS_JSON_PATH,
+      JSON.stringify(reassignIDs<UserModel>(cleanUsers), null, 2),
+    );
+    return data;
   }
 
   async updateUser(data: UserModel) {
+    schemeValidation<UserModel>(UserScheme, data);
+
+    const tmpUser: UserModel = structuredClone(data);
+    let rawData: string;
+
     try {
-      schemeValidation<UserModel>(UserScheme, data);
-
-      const tmpUser: UserModel = structuredClone(data);
-      let rawData: string;
-
-      try {
-        rawData = (await FsService.readFile(USERS_JSON_PATH)) as string;
-      } catch (readError: any) {
-        if (readError.code === "ENOENT") {
-          rawData = "[]";
-        } else {
-          throw readError;
-        }
-      }
-
-      let wasUpdate: boolean;
-      const users: UserModel[] = JSON.parse(rawData);
-      const updatedUsers: UserModel[] = users.map((user) => {
-        if (user.id === tmpUser.id) {
-          wasUpdate = true;
-          return tmpUser;
-        } else {
-          return user;
-        }
-      });
-
-      if (!wasUpdate) {
-        throw new Error(`User with id "${tmpUser.id}" was not found`);
+      rawData = (await FsService.readFile(USERS_JSON_PATH)) as string;
+    } catch (readError: any) {
+      if (readError.code === "ENOENT") {
+        rawData = "[]";
       } else {
-        await FsService.writeFile(
-          USERS_JSON_PATH,
-          JSON.stringify(reassignIDs<UserModel>(updatedUsers), null, 2),
-        );
+        throw readError;
       }
-
-      return data;
-    } catch (error) {
-      throw error;
     }
+
+    let wasUpdate: boolean;
+    const users: UserModel[] = JSON.parse(rawData);
+    const updatedUsers: UserModel[] = users.map((user) => {
+      if (user.id === tmpUser.id) {
+        wasUpdate = true;
+        return tmpUser;
+      } else {
+        return user;
+      }
+    });
+
+    if (!wasUpdate) {
+      throw new Error(`User with id "${tmpUser.id}" was not found`);
+    } else {
+      await FsService.writeFile(
+        USERS_JSON_PATH,
+        JSON.stringify(reassignIDs<UserModel>(updatedUsers), null, 2),
+      );
+    }
+
+    return data;
   }
 
   async deleteUser(id: string) {
     if (!id || id.length === 0) return null;
 
+    let rawData: string;
+
     try {
-      let rawData: string;
-
-      try {
-        rawData = (await FsService.readFile(USERS_JSON_PATH)) as string;
-      } catch (readError: any) {
-        if (readError.code === "ENOENT") {
-          rawData = "[]";
-        } else {
-          throw readError;
-        }
-      }
-
-      let deleteUser: UserModel;
-      const users: UserModel[] = JSON.parse(rawData);
-      let updatedUsers: UserModel[] = [];
-      users.map((user) => {
-        if (user.id === id) {
-          deleteUser = user;
-        } else {
-          updatedUsers.push(user);
-        }
-      });
-
-      if (!deleteUser) {
-        throw new Error(`User with id '${id}' was not found`);
+      rawData = (await FsService.readFile(USERS_JSON_PATH)) as string;
+    } catch (readError: any) {
+      if (readError.code === "ENOENT") {
+        rawData = "[]";
       } else {
-        await FsService.writeFile(
-          USERS_JSON_PATH,
-          JSON.stringify(reassignIDs<UserModel>(updatedUsers), null, 2),
-        );
+        throw readError;
       }
-
-      return deleteUser;
-    } catch (error) {
-      throw error;
     }
+
+    let deleteUser: UserModel;
+    const users: UserModel[] = JSON.parse(rawData);
+    let updatedUsers: UserModel[] = [];
+    users.map((user) => {
+      if (user.id === id) {
+        deleteUser = user;
+      } else {
+        updatedUsers.push(user);
+      }
+    });
+
+    if (!deleteUser) {
+      throw new Error(`User with id '${id}' was not found`);
+    } else {
+      await FsService.writeFile(
+        USERS_JSON_PATH,
+        JSON.stringify(reassignIDs<UserModel>(updatedUsers), null, 2),
+      );
+    }
+
+    return deleteUser;
   }
 
   async deleteAllUsers() {
+    let rawData: string;
+
     try {
-      let rawData: string;
-
-      try {
-        rawData = await FsService.readFile(USERS_JSON_PATH);
-      } catch (readError: any) {
-        if (readError.code === "ENOENT") {
-          rawData = "[]";
-        } else {
-          throw readError;
-        }
-      }
-
-      const users: UserModel[] = JSON.parse(rawData) as UserModel[];
-
-      if (!users || users.length === 0) {
-        throw new Error("The is no users to delete");
+      rawData = await FsService.readFile(USERS_JSON_PATH);
+    } catch (readError: any) {
+      if (readError.code === "ENOENT") {
+        rawData = "[]";
       } else {
-        await FsService.writeFile(USERS_JSON_PATH, JSON.stringify([], null, 2));
+        throw readError;
       }
-
-      return users;
-    } catch (error) {
-      throw error;
     }
+
+    const users: UserModel[] = JSON.parse(rawData) as UserModel[];
+
+    if (!users || users.length === 0) {
+      throw new Error("The is no users to delete");
+    } else {
+      await FsService.writeFile(USERS_JSON_PATH, JSON.stringify([], null, 2));
+    }
+
+    return users;
   }
 }
