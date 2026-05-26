3da69cc2c252b3fa79ea0c40971739e350a32410
Nik <laveosa@yahoo.com>
2026-05-26 18:05:30 +0300

    Update - run log-server logic

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
 import { runApiServer } from "#src/servers/api-server.js";
 import { runDbServer } from "#src/servers/db-server.js";
+import { runLogService } from "#src/servers/log-server.js";
 
 runDbServer();
 runApiServer();
+runLogService();
